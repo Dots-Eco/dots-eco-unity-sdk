4f45c12227c2cc23d698015d9c8b0564c1de48f7
Erez <erez@dots.eco>
2023-08-18 11:00:53 +0700

    added ability to create certificates, screenshots of capabilties

--- Assets/Editor/CreateSanboxCertificateWindow.cs
+++ Assets/Editor/CreateSanboxCertificateWindow.cs
@@ -1,24 +1,39 @@
-﻿using UnityEditor;
+﻿using System.Text.RegularExpressions;
+using DotsEcoCertificateSDK;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class CreateSanboxCertificateWindow : EditorWindow
 {
-    private string appToken = "";
-    private string impactQty = "";
-    private string allocationId = "";
-    private string remoteUserId = "";
-    private string nameOnCertificate = "";
-    private string remoteUserEmail = "";
-    private string certificateDesign = "";
-    private string sendCertificateByEmail = "";
-    private string certificateInfo = "";
-    private string langCode = "";
-    private string currency = "";
+    private bool isSendingRequest = false;
+    private CertificateService certificateService;
+    private Texture2D certificateImage;
     private string jsonResponse = "";
+
+
+    private string appToken = "";
+    private string impactQty = "1";
+    private string allocationId = "5";
+    private string remoteUserId = "ecohero";
+    private string nameOnCertificate = "Eco Hero";
+    private string remoteUserEmail = "eco@dots.eco";
+    private string certificateDesign = "silver";
+    private string sendCertificateByEmail = "yes";
+    private string certificateInfo = "save the planet";
+    private string langCode = "EN";
+    private string currency = "USD";
 
     public static void ShowWindow()
     {
         GetWindow<CreateSanboxCertificateWindow>("Create Certificate in Sandbox");
+    }
+
+    private void Awake()
+    {
+        string authToken = EditorPrefs.GetString("DotsEco_AuthToken");
+        appToken = EditorPrefs.GetString("DotsEco_SandboxAppToken");
+        certificateService = new CertificateService(authToken);
     }
 
     private void OnGUI()
@@ -26,7 +41,10 @@ public class CreateSanboxCertificateWindow : EditorWindow
         EditorGUILayout.LabelField("Create Certificate in Sandbox", EditorStyles.boldLabel);
 
         EditorGUILayout.Space();
-        appToken = EditorGUILayout.TextField("App Token (required):", appToken);
+
+        EditorGUI.BeginDisabledGroup(true);
+        EditorGUILayout.TextField("App Token:", appToken);
+        EditorGUI.EndDisabledGroup();
         impactQty = EditorGUILayout.TextField("Impact Quantity (required):", impactQty);
         allocationId = EditorGUILayout.TextField("Allocation ID (required):", allocationId);
         remoteUserId = EditorGUILayout.TextField("Remote User ID (required):", remoteUserId);
@@ -42,14 +60,10 @@ public class CreateSanboxCertificateWindow : EditorWindow
         bool canSend = !string.IsNullOrEmpty(appToken) && !string.IsNullOrEmpty(impactQty) &&
                        !string.IsNullOrEmpty(allocationId) && !string.IsNullOrEmpty(remoteUserId);
 
-        EditorGUI.BeginDisabledGroup(!canSend);
-        if (GUILayout.Button("Send"))
+        EditorGUI.BeginDisabledGroup(isSendingRequest);
+        if (GUILayout.Button(isSendingRequest ? "Sending..." : "Send"))
         {
-            // TODO: Implement the SDK call for creating certificate here.
-            // For now, this is just a mockup:
-            jsonResponse = "{ \"response\": \"This is a mockup JSON response.\" }";
-
-            // When you implement the SDK call, use the resulting JSON to populate the jsonResponse variable.
+            SendCreateCertificateRequest();
         }
         EditorGUI.EndDisabledGroup();
 
@@ -57,5 +71,98 @@ public class CreateSanboxCertificateWindow : EditorWindow
 
         EditorGUILayout.LabelField("Response:", EditorStyles.boldLabel);
         EditorGUILayout.TextArea(jsonResponse, GUILayout.Height(100));
+
+        if (certificateImage)
+        {
+            GUILayout.Label(certificateImage, GUILayout.Height(400), GUILayout.ExpandWidth(true));
+        }
+    }
+    private void SendCreateCertificateRequest()
+    {
+        isSendingRequest = true;
+
+        string appToken = EditorPrefs.GetString("DotsEco_SandboxAppToken");
+
+        if (string.IsNullOrEmpty(appToken))
+        {
+            jsonResponse = "Error: Authentication token is missing. Please provide it in the configuration window.";
+            return;
+        }
+
+        CreateCertificateRequestBuilder builder = new CreateCertificateRequestBuilder(appToken, int.Parse(impactQty), int.Parse(allocationId), remoteUserId)
+            .WithNameOnCertificate(nameOnCertificate)
+            .WithRemoteUserEmail(remoteUserEmail)
+            .WithCertificateDesign(certificateDesign)
+            .WithSendCertificateByEmail(sendCertificateByEmail)
+            .WithCertificateInfo(certificateInfo)
+            .WithLangCode(langCode)
+            .WithCurrency(currency);
+
+        UnityWebRequest request = certificateService.CreateCertificateRequest(builder);
+        var operation = request.SendWebRequest();
+
+        EditorApplication.update += CheckRequestCompletion;
+
+        void CheckRequestCompletion()
+        {
+            if (!operation.isDone) return;
+
+            EditorApplication.update -= CheckRequestCompletion;
+
+            isSendingRequest = false;
+
+            if (request.result == UnityWebRequest.Result.ConnectionError || request.result == UnityWebRequest.Result.ProtocolError)
+            {
+                jsonResponse = "Error: " + request.error;
+            }
+            else
+            {
+                jsonResponse = request.downloadHandler.text;
+                 CertificateResponse certificate = JsonUtility.FromJson<CertificateResponse>(request.downloadHandler.text);
+                
+            }
+
+            if (request.result == UnityWebRequest.Result.ConnectionError || request.result == UnityWebRequest.Result.ProtocolError)
+            {
+                jsonResponse = "Error: " + request.error;
+            }
+            else
+            {
+                jsonResponse = request.downloadHandler.text;
+                CertificateResponse certificate = JsonUtility.FromJson<CertificateResponse>(request.downloadHandler.text);
+
+                if (!string.IsNullOrEmpty(certificate.certificate_image_url))
+                {
+                    FetchImage(Regex.Unescape(certificate.certificate_image_url));
+                }
+            }
+            Repaint();
+            Repaint();
+        }
+    }
+    private void FetchImage(string imageUrl)
+    {
+        UnityWebRequest imageRequest = UnityWebRequestTexture.GetTexture(imageUrl);
+        var imageOperation = imageRequest.SendWebRequest();
+
+        EditorApplication.update += CheckImageRequestCompletion;
+
+        void CheckImageRequestCompletion()
+        {
+            if (!imageOperation.isDone) return;
+
+            EditorApplication.update -= CheckImageRequestCompletion;
+
+            if (imageRequest.result == UnityWebRequest.Result.ConnectionError || imageRequest.result == UnityWebRequest.Result.ProtocolError)
+            {
+                Debug.LogError("Error loading image: " + imageRequest.error);
+            }
+            else
+            {
+                certificateImage = DownloadHandlerTexture.GetContent(imageRequest);
+            }
+
+            Repaint();
+        }
     }
 }
